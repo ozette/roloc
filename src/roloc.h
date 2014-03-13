e0a3d5c1164b7708fa89680993300ad42f8d5b7b
Joey Toppin <mantissa.sys@gmail.com>
2014-03-13 14:18:17 +0100

    Added copyright/license notice to roloc.h

--- src/roloc.h
+++ src/roloc.h
@@ -1,3 +1,12 @@
+/*
+* roloc: a color cook program, color calculator and palette generator.
+*
+* Copyright (C) 2013 Joey Toppin <mantissa.sys@gmail.com>
+*
+* See COPYING at the top-level directory of this distribution or:
+* <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef ROLOC_H_
 #define ROLOC_H_
 typedef struct roloc_rgb {
